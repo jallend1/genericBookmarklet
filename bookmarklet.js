57b43462772d5dbb217fc342dca5b19fd3f38e51
jallend1 <jallend1@gmail.com>
2023-07-18 21:25:37 -0700

    Selects an element and copies it to clipboard

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,7 +1,20 @@
 javascript: (function () {
+  const selectMostRecentItem = () => {
+    const items = document.querySelectorAll('.item');
+    const mostRecentItem = items[items.length - 1];
+    return mostRecentItem;
+  };
+
+  const copyItemToClipboard = () => {
+    const mostRecentItem = selectMostRecentItem();
+    const itemText = mostRecentItem.innerText;
+    navigator.clipboard.writeText(itemText);
+  };
+
   document.addEventListener('keydown', function (e) {
     if (e.key === 'Enter') {
       e.preventDefault();
+      copyItemToClipboard();
       window.open('https://www.google.com', '_blank');
     }
   });
