8c425e9ce72c144a9bc98ba85de0d0af3cf777f5
jallend1 <jallend1@gmail.com>
2023-07-19 20:34:21 -0700

    Bookmarklet successfully targets the most recent scanned item and loads the page. Adds a minified version

--- bookmarklet.js
+++ bookmarklet.js
@@ -1,21 +1,22 @@
 javascript: (function () {
   const selectMostRecentItem = () => {
-    const items = document.querySelectorAll('.item');
-    const mostRecentItem = items[items.length - 1];
-    return mostRecentItem;
+    const callNumberField = document.querySelector('#staff-content-container > ng-component > eg-grid > div > eg-grid-body > div > div:nth-child(1) > div:nth-child(6) > eg-grid-body-cell > span');
+    const callNumber = callNumberField.innerText;
+    const callNumberWithoutPrefix = callNumber.slice(2);
+    return callNumberWithoutPrefix;
   };
 
   const copyItemToClipboard = () => {
-    const mostRecentItem = selectMostRecentItem();
-    const itemText = mostRecentItem.innerText;
-    navigator.clipboard.writeText(itemText);
+    // const mostRecentItem = selectMostRecentItem();
+    const requestNumber = selectMostRecentItem();
+    navigator.clipboard.writeText(requestNumber);
   };
 
   document.addEventListener('keydown', function (e) {
     if (e.key === 'Enter') {
       e.preventDefault();
       copyItemToClipboard();
-      window.open('https://www.google.com', '_blank');
+      window.open('https://regal-pithivier-abc90d.netlify.app/' + selectMostRecentItem(), '_blank');
     }
   });
 })();
