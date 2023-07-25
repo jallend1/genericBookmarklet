9a5797fbfae4bdef0fc0bbe2a6391285ac72a5a6
Jason Allen <39307371+jallend1@users.noreply.github.com>
2023-07-25 16:03:37 -0700

    Converts it to keyup event and removes preventDefault to allow enter to perform as expected

--- bookmarklet-min.js
+++ bookmarklet-min.js
@@ -1,3 +1,27 @@
-javascript: (function () { const e = () => document.querySelector("#staff-content-container > ng-component > eg-grid > div > eg-grid-body > div > div:nth-child(1) > div:nth-child(6) > eg-grid-body-cell > span").innerText.slice(2); document.addEventListener("keydown", (function (n) { "Enter" === n.key && (n.preventDefault(), (() => { const n = e(); navigator.clipboard.writeText(n) })(), window.open("https://regal-pithivier-abc90d.netlify.app/" + e(), "_blank")) })); })();
-javascript: (function () { const e = () => document.querySelector("#staff-content-container > ng-component > eg-grid > div > eg-grid-body > div > div:nth-child(1) > div:nth-child(6) > eg-grid-body-cell > span").innerText.slice(2); document.addEventListener("keydown", (function (t) { "Enter" === t.key && (t.preventDefault(), (() => { const t = e(); navigator.clipboard.writeText(t) })(), window.open("https://regal-pithivier-abc90d.netlify.app/" + e(), "_blank")) })), (() => { const e = document.createElement("div"); e.id = "bookmarklet", e.style = "position: fixed; bottom: 0; left: 0; width: 100%; height: 100px; background-color: red; z-index: 9999;", e.textContent = "Hello, world!", document.body.appendChild(e) })(); })();
-javascript: (function () { const e = () => document.querySelector("#staff-content-container > ng-component > eg-grid > div > eg-grid-body > div > div:nth-child(1) > div:nth-child(6) > eg-grid-body-cell > span").innerText.slice(2); document.addEventListener("keydown", (function (t) { "Enter" === t.key && (t.preventDefault(), (() => { const t = e(); navigator.clipboard.writeText(t) })(), window.open("https://regal-pithivier-abc90d.netlify.app/" + e(), "_blank")) })), (() => { const e = document.createElement("div"); e.id = "bookmarklet", e.style = "position: fixed; bottom: 0; left: 0; width: 100%; height: 50px; background-color: red; z-index: 9999; text-align: center;", e.textContent = "Return slip mode activated! Refresh to exit.", document.body.appendChild(e) })(); })();
+javascript: !(function () {
+  const e = () =>
+    document
+      .querySelector(
+        '#staff-content-container > ng-component > eg-grid > div > eg-grid-body > div > div:nth-child(1) > div:nth-child(6) > eg-grid-body-cell > span'
+      )
+      .innerText.slice(2);
+  document.addEventListener('keyup', function (t) {
+    'Enter' === t.key &&
+      ((() => {
+        const t = e();
+        navigator.clipboard.writeText(t);
+      })(),
+      window.open(
+        'https://regal-pithivier-abc90d.netlify.app/' + e(),
+        '_blank'
+      ));
+  }),
+    (() => {
+      const e = document.createElement('div');
+      (e.id = 'bookmarklet'),
+        (e.style =
+          'position: fixed; bottom: 0; left: 0; width: 100%; height: 50px; background-color: red; z-index: 9999; text-align: center;'),
+        (e.textContent = 'Return slip mode activated! Refresh to exit.'),
+        document.body.appendChild(e);
+    })();
+})();
--- bookmarklet.js
+++ bookmarklet.js
@@ -1,6 +1,8 @@
 javascript: (function () {
   const selectMostRecentItem = () => {
-    const callNumberField = document.querySelector('#staff-content-container > ng-component > eg-grid > div > eg-grid-body > div > div:nth-child(1) > div:nth-child(6) > eg-grid-body-cell > span');
+    const callNumberField = document.querySelector(
+      '#staff-content-container > ng-component > eg-grid > div > eg-grid-body > div > div:nth-child(1) > div:nth-child(6) > eg-grid-body-cell > span'
+    );
     const callNumber = callNumberField.innerText;
     const callNumberWithoutPrefix = callNumber.slice(2);
     return callNumberWithoutPrefix;
@@ -14,16 +16,19 @@ javascript: (function () {
   const insertDivAtBottom = () => {
     const div = document.createElement('div');
     div.id = 'bookmarklet';
-    div.style = 'position: fixed; bottom: 0; left: 0; width: 100%; height: 50px; background-color: red; z-index: 9999; text-align: center;';
+    div.style =
+      'position: fixed; bottom: 0; left: 0; width: 100%; height: 50px; background-color: red; z-index: 9999; text-align: center;';
     div.textContent = 'Return slip mode activated! Refresh to exit.';
     document.body.appendChild(div);
   };
 
-  document.addEventListener('keydown', function (e) {
+  document.addEventListener('keyup', function (e) {
     if (e.key === 'Enter') {
-      e.preventDefault();
       copyItemToClipboard();
-      window.open('https://regal-pithivier-abc90d.netlify.app/' + selectMostRecentItem(), '_blank');
+      window.open(
+        'https://regal-pithivier-abc90d.netlify.app/' + selectMostRecentItem(),
+        '_blank'
+      );
     }
   });
 
